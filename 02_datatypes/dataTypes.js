3eb373967ca0795b8f7b0ca7e5d06965bc0d023a
developerjihad99 <developerjihad99@gmail.com>
2026-05-08 23:36:11 +0600

    Starting JS Learning with chai aur code hitesh rana

---
+++ 02_datatypes/dataTypes.js
@@ -0,0 +1,22 @@
+// alert( 3 + 3); we are using nodeJs, not browser
+
+let name = "Jihad"; // string
+let age = 30; // number
+let isStudent = true; // boolean
+let state = null; // null
+let city; // undefined
+
+//number => 2 to the power 53 
+//bigint
+//string => ""
+//boolean => true or false
+//null => absence of value
+//undefined => variable declared but not assigned a value
+//symbol => unique identifier
+//object
+
+console.log(typeof "Jihad");
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof state);
+console.log(typeof city);
---
+++ variables/variables.js
@@ -0,0 +1,22 @@
+const account_Id = 14432
+let accountEmail = "hassanjihad414@gmail.com"
+var accountPassword = "Jihad8448"
+accountCity = "Dhaka"
+
+// account_Id = 2 //Not allowed to change because it's a constant
+
+console.log(account_Id);
+
+accountEmail = "mdjihadhassan59@gmail.com"
+accountPassword = "465626"
+accountCity = "Jhenaidah"
+let accountState;
+
+console.log(accountEmail);
+
+console.table([account_Id, accountEmail, accountPassword, accountCity, accountState]);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and function scope
+*/
