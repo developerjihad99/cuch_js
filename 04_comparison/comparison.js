1874b22907dc4e07e832a758da173018d77201b8
developerjihad99 <developerjihad99@gmail.com>
2026-05-08 23:58:15 +0600

    Comparison in JS

---
+++ 04_comparison/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //comparison convert null to a number, treating it as 0, so this is false
+console.log(null == 0);// equality operator does not convert null to a number, so this is false
+console.log(null >= 0); //comparison convert null to a number, treating it as 0, so this is true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === 
+
+console.log("2" === 2);
+console.log(2 === 2);
+
