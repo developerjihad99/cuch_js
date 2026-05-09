7b7bba26fd2d66c71497617277a7ed8a9c08967f
developerjihad99 <developerjihad99@gmail.com>
2026-05-09 10:35:01 +0600

    data types summary

--- 04_comparison/comparison.js
+++ 04_comparison/comparison.js
@@ -9,6 +9,8 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+/*
+
 console.log(null > 0); //comparison convert null to a number, treating it as 0, so this is false
 console.log(null == 0);// equality operator does not convert null to a number, so this is false
 console.log(null >= 0); //comparison convert null to a number, treating it as 0, so this is true
@@ -16,7 +18,7 @@ console.log(null >= 0); //comparison convert null to a number, treating it as 0,
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
-
+*/
 
 // === 
 
